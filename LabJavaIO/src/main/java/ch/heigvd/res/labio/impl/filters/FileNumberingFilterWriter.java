383416524227b8c58e82bfc874dbae007fa54a99
Doriane Kaffo <dorianekaffo@air-de-doriane.einet.ad.eivd.ch>
2019-03-13 18:59:53 +0100

    FileNumberingFilterWriter, FileTransformer and NoOpFileTransformer are ok

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -17,6 +17,9 @@ import java.util.logging.Logger;
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
+  private int numberOfLine = 1;
+  private boolean r = false; // detection of \r before \n
+
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
   public FileNumberingFilterWriter(Writer out) {
@@ -25,17 +28,42 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    write(str.toCharArray(), off, len);// convertion of string into chararray
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for (int i = 0; i < len; ++i) {
+      this.write(cbuf[off + i]);
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if (numberOfLine == 1) { //on first line, wee add the tabulation on the numberOfLine
+      out.write(numberOfLine + "\t");
+      ++numberOfLine;
+    }
+    if (r == true) { // if last caracter was a '\r', we check if there is a '\n'
+      if ((char) c != '\n') {
+        out.write(numberOfLine + "\t");
+        ++numberOfLine;
+      }
+    }
+
+    r = false;
+
+    if ((char) c == '\r') {
+      out.write('\r');
+      r = true;
+    } else {
+      out.write((char) c);
+      if (c == '\n') {
+        out.write(numberOfLine + "\t");
+        ++numberOfLine;
+      }
+    }
   }
 
 }
+
--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
@@ -58,6 +58,10 @@ public abstract class FileTransformer implements IFileVisitor {
        * writer has been decorated by the concrete subclass!). You need to write a loop to read the
        * characters and write them to the writer.
        */
+      int characters;
+      while ((characters = reader.read()) != -1) {
+        writer.write((char) characters);
+      }
       
       reader.close();
       writer.flush();
